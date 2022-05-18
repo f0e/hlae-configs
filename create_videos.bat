6873194e45e3793eea8dd26f67e550c37197d2b9
f0e <7321764+f0e@users.noreply.github.com>
2022-05-19 01:18:34 +1000

    create_videos: get output extension from input

--- create_videos.bat
+++ create_videos.bat
@@ -20,13 +20,13 @@ for /d %%f in ("take*") do (
 				REM line 2: get input files - video and audio
 				REM line 3: use video from the first file, (optional) audio from the second
 				REM line 4: copy the video, don't re-encode
-				REM line 5: output file
+				REM line 5: output file (%%~xv = video extension)
 				ffmpeg ^
 					-loglevel error -hide_banner -stats ^
 					-i "%%f/%%n/%%v" -i "%%f/%%a" ^
 					-map 0:v -map 1:a? ^
 					-c:v copy ^
-					compiled/%%f_%%n.mp4
+					compiled/%%f_%%n%%~xv
 			)
 		)
 	)
